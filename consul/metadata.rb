16e14c24c9cef013e7051cefb5a5fa5140b8097d
Rajeshwari Makond <rajeshwari.makond@reancloud.com>
2017-08-10 15:52:13 +0530

    Update metadata.rb

--- consul/metadata.rb
+++ consul/metadata.rb
@@ -1,6 +1,6 @@
 name 'consul'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Rajeshwari'
+maintainer_email 'rajeshwari.makond@reancloud.com'
 license 'All Rights Reserved'
 description 'Installs/Configures consul'
 long_description 'Installs/Configures consul'
@@ -11,10 +11,10 @@ chef_version '>= 12.1' if respond_to?(:chef_version)
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/consul/issues'
+issues_url 'https://github.com/aashishmodak/cookbooksrepo/consul/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/consul'
+source_url 'https://github.com/aashishmodak/cookbooksrepo/consul'
