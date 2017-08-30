873525b66b6f37422c89e3d8b85eca98f3e17f0f
root <root@ip-172-31-11-5.ec2.internal>
2017-08-30 10:28:24 +0000

    Modified MongoDB cookbook

--- MongoDB/recipes/default.rb
+++ MongoDB/recipes/default.rb
@@ -3,8 +3,34 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
 #include_recipe 'MongoDB::importpkey'
-include_recipe 'MongoDB::createfile'
+execute 'import public key cmd' do
+    command "sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10"
+    action :run
+end
+
+#include_recipe 'MongoDB::createfile'
+execute 'create list of files' do
+   command 'echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list'
+   action :run
+   not_if { File.exist?('/etc/apt/sources.list.d/mongodb-org-3.0.list') }
+end
+
 #include_recipe 'MongoDB::update'
+execute "update-upgrade" do
+  command "apt-get update -y"
+  action :run
+end
+
 #include_recipe 'MongoDB::install'
+execute 'install MongoDB' do
+     command "apt-get install mongodb-org -y"
+     action :run
+end
+
 #include_recipe 'MongoDB::MongoDBstatus'
+execute 'check status of MongoDB' do
+   command "service mongod status"
+   action :run
+end
