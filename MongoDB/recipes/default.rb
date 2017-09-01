e99d6c914b96740b3a495fb90756b086aeab10cb
root <root@ip-172-31-11-5.ec2.internal>
2017-09-01 10:46:54 +0000

    updated cookbook as per comments

--- MongoDB/recipes/default.rb
+++ MongoDB/recipes/default.rb
@@ -4,33 +4,30 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-#include_recipe 'MongoDB::importpkey'
+#'MongoDB::importpkey'
 execute 'import public key cmd' do
     command "sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10"
     action :run
+    not_if 'apt-key list | grep 7F0CEB10'
 end
 
-#include_recipe 'MongoDB::createfile'
+#'MongoDB::createfile'
 execute 'create list of files' do
    command 'echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list'
    action :run
    not_if { File.exist?('/etc/apt/sources.list.d/mongodb-org-3.0.list') }
 end
 
-#include_recipe 'MongoDB::update'
-execute "update-upgrade" do
-  command "apt-get update -y"
-  action :run
+#'MongoDB::update'
+apt_update 'update' do
+notifies :run, "execute[create list of files]"
 end
 
-#include_recipe 'MongoDB::install'
-execute 'install MongoDB' do
-     command "apt-get install mongodb-org -y"
-     action :run
+#'MongoDB::installs, enable and start'
+package "mongodb-org" do
+  action :install
 end
 
-#include_recipe 'MongoDB::MongoDBstatus'
-execute 'check status of MongoDB' do
-   command "service mongod status"
-   action :run
+service 'mongod' do
+   action [:enable, :start]
 end
