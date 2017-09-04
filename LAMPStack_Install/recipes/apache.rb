7434c234bb9cc130dfa364ece0abb0abb3dd45d4
root <root@ip-172-31-11-5.ec2.internal>
2017-09-04 05:56:14 +0000

    removed LAMP Stack files

--- LAMPStack_Install/recipes/apache.rb
+++
@@ -1,9 +0,0 @@
-#Install & Anable Apache
-
-package "apache2" do
-  action :install
-end
-
-service "apache2" do
-  action [:enable, :start]
-end
--- LAMPStack_Install/recipes/mysql.rb
+++
@@ -1,9 +0,0 @@
-#Install Mysql
-
-
-mysql_service 'foo' do
-  port '3306'
-  version '5.5'
-  initial_root_password 'P@ssword220413'
-  action [:create, :start]
-end
