fb9c4b9aed9c75eb8e6f4778bc121ee6fa68e618
root <root@ip-172-31-11-5.ec2.internal>
2017-09-28 11:33:29 +0000

    updated cookbook

--- HashicorpVault/recipes/default.rb
+++ HashicorpVault/recipes/default.rb
@@ -1,12 +1,21 @@
-#
+vault read secret/rajeshwari#
 # Cookbook:: HashicorpVault
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
 cookbook_file '/usr/bin/vault' do
 source 'vault'
 user 'root'
 group 'root'
 mode '0755'
 action :create
+end
+
+bash 'command' do
+  code <<-EOH
+    vault server -dev &>/dev/null &
+    echo "export VAULT_ADDR='http://127.0.0.1:8200'" >> /etc/environment
+    source /etc/environment
+   EOH
 end
